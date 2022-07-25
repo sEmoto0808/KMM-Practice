533020f494cf3e024063823aa76a56dfe2588aab
Sho Emoto <dd2d00seg1@gmail.com>
2022-07-25 21:57:33 +0900

    Android: 通信処理

--- KMMFirstApp/androidApp/src/main/java/com/example/kmmfirstapp/android/MainActivity.kt
+++ KMMFirstApp/androidApp/src/main/java/com/example/kmmfirstapp/android/MainActivity.kt
@@ -1,20 +1,36 @@
 package com.example.kmmfirstapp.android
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.kmmfirstapp.Greeting
 import android.widget.TextView
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
 
 fun greet(): String {
     return Greeting().greeting()
 }
 
 class MainActivity : AppCompatActivity() {
+    private val mainScope = MainScope()
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val tv: TextView = findViewById(R.id.text_view)
-        tv.text = greet()
+//        tv.text = greet()
+
+        mainScope.launch {
+            kotlin.runCatching {
+                Greeting().getHtml()
+            }.onSuccess {
+                tv.text = it
+            }.onFailure {
+                tv.text = "Error: ${it.localizedMessage}"
+            }
+        }
     }
 }
