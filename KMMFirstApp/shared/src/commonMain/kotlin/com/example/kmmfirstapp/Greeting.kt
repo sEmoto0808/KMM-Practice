20ee8a131d48b07f4af2c4a734e91da766e7ed03
Sho Emoto <dd2d00seg1@gmail.com>
2022-07-25 23:05:50 +0900

    add ktor-client-serialization

--- KMMFirstApp/shared/src/commonMain/kotlin/com/example/kmmfirstapp/Greeting.kt
+++ KMMFirstApp/shared/src/commonMain/kotlin/com/example/kmmfirstapp/Greeting.kt
@@ -9,7 +9,10 @@ class Greeting {
         return "Hello, ${Platform().platform}!"
     }
 
-    private val client = HttpClient()
+    private val client = HttpClient() {
+
+    }
+
     suspend fun getHtml(): String {
         val response = client.get("https://ktor.io/docs")
         return response.bodyAsText()
