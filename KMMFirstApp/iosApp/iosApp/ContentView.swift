cc6d7a1f6a5d84b1b92c05e7c2b35195ec57cf84
Sho Emoto <dd2d00seg1@gmail.com>
2022-07-25 22:05:27 +0900

    iOS: 通信処理

--- KMMFirstApp/iosApp/iosApp/ContentView.swift
+++ KMMFirstApp/iosApp/iosApp/ContentView.swift
@@ -2,11 +2,24 @@ import SwiftUI
 import shared
 
 struct ContentView: View {
-	let greet = Greeting().greeting()
+	let greeting = Greeting()
+
+    @State var greet = "Loading"
 
 	var body: some View {
 		Text(greet)
+            .onAppear(perform: load)
 	}
+
+    func load() {
+        greeting.getHtml { result, error in
+            if let result = result {
+                greet = result
+            } else if let error = error {
+                greet = "Error: \(error)"
+            }
+        }
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
